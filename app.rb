d43169ca2b9de4aa70c83385f608b38340b22577
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-02-15 18:11:15 +0200

    Some changes

--- app.rb
+++ app.rb
@@ -1,42 +1,96 @@
 # Required gems and modules
 require 'rubygems'
-require 'sinatra'
+require 'sinatra/base'
 require 'mongoid'
 require 'haml'
+require './models/word'
 
-APP_NAME = "Vocabulatron"
+class Vocabulatron < Sinatra::Base
+  # directory path settings relative to app file
+  set :root, Vocabulatron.root
+  set :public_folder, Proc.new { File.join(root, 'public') }
+  set :method_override, true
+  
+#  configure do
+#     Mongoid.configure do |config|
+#      name = "mongoid_dev"
+#      host = "localhost"
+#      config.master = Mongo::Connection.new.db(name)
+#    end
+#  end
 
-# MongoDB configuration
-Mongoid.configure do |config|
-  if ENV['MONGOHQ_URL']
-    conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-    uri = URI.parse(ENV['MONGOHQ_URL'])
-    config.master = conn.db(uri.path.gsub(/^\//, ''))
-  else
-    config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('test')
-  end
-end
+  APP_NAME = "Vocabulatron"
 
-
-
-
-# Models
-class Counter
-    include Mongoid::Document
-    
-    field :count, :type => Integer
-    
-    def self.increment
-      c = first || new({:count => 0})
-      c.inc(:count, 1)
-      c.save
-      c.count
+  # MongoDB configuration
+  Mongoid.configure do |config|
+    if ENV['MONGOHQ_URL']
+      conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+      uri = URI.parse(ENV['MONGOHQ_URL'])
+      config.master = conn.db(uri.path.gsub(/^\//, ''))
+    else
+      config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('vocabulatron_development')
     end
+  end
+  
+  def initialize
+    super
+  end
+
+  get '/' do
+    redirect '/words'
+  end
+  
+  # list all words
+  get '/words' do
+    @words = Word.all
+    haml :'words/index', :locals => { :title => "Vocabulary", :words => @words }
+  end
+  
+  # view a word
+  get '/words.:id' do |id|
+    @word = Word.find(id)
+    haml :'words/show', :locals => { :title => "Word", :word => @word}
+  end  
+  
+  # new word
+  get '/words/new' do
+    haml :'words/new', :locals => { :title => "New Word"}
+  end  
+  
+  post '/words' do
+    @word = params[:word]
+    @word = Word.new(params[:word])
+    if @word.save
+      puts 'Correct Word'
+      redirect '/words'
+    else
+      puts "Error(s): ", @word.errors.map {|k,v| "#{k}: #{v}"}
+      haml :'words/error', :locals => { :errors => @word.errors } 
+    end
+  end
+  
+  # edit word
+  get '/words.:id./edit' do |id|
+    @word = Word.find(id)
+    haml :'words/edit', :locals => { :title => "Edit Word", :word => @word}
+  end
+  
+  put '/words.:id' do |id|
+    @word = Word.find(id)
+    @word.update_attributes(params[:word])
+    redirect '/words.'+id
+  end
+  
+  #delete blog
+  delete '/words.:id' do |id|
+    @word = Word.find(id)
+    @word.delete
+    redirect '/'
+  end
+  
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+  
 end
 
-# Controllers
-get '/' do
-  @data = "Hello visitor #" + Counter.increment.to_s
-  haml :index
-end
 
